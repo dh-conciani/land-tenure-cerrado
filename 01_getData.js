e0fdf6162c79e7ea5591c3bea5e60788d4222fdb
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2022-09-21 16:10:54 -0300

    Update 01_getData.js

--- 01_getData.js
+++ 01_getData.js
@@ -24,6 +24,104 @@ var states = ee.Image('projects/mapbiomas-workspace/AUXILIAR/estados-2016-raster
 Map.addLayer(tenure.randomVisualizer(), {}, 'tenure');
 Map.addLayer(states.randomVisualizer(), {}, 'states');
 
+// define states to be computed (pixel-values)
+var states_list = [21, 22];
+//17, 29 ,51, 15, 52, 53, 31, 50, 35, 41, 11];
+
+// define the years to compute area
+var years = ee.List.sequence({'start': 1985, 'end': 1986, 'step': 1}).getInfo();
+
+// define area unit (hectares)
+var pixelArea = ee.Image.pixelArea().divide(10000);
+
+// for each state
+states_list.forEach(function(state_i) {
+
+  
+});
+
+
+
+
+  
+// convert a complex object to a simple feature collection 
+var convert2table = function (obj) {
+    obj = ee.Dictionary(obj);
+      var territory = obj.get('territory');
+      var classesAndAreas = ee.List(obj.get('groups'));
+      
+      var tableRows = classesAndAreas.map(
+          function (classAndArea) {
+              classAndArea = ee.Dictionary(classAndArea);
+              var classId = classAndArea.get('class');
+              var area = classAndArea.get('sum');
+              var tableColumns = ee.Feature(null)
+                  .set('territory', territory)
+                  .set('class_id', classId)
+                  .set('area', area)
+                  .set('file', basename);
+                  
+              return tableColumns;
+          }
+      );
+  
+      return ee.FeatureCollection(ee.List(tableRows));
+  };
+  
+  // compute the area
+  var calculateArea = function (image, territory, geometry) {
+      var territotiesData = pixelArea.addBands(territory).addBands(image)
+          .reduceRegion({
+              reducer: ee.Reducer.sum().group(1, 'class').group(1, 'territory'),
+              geometry: geometry,
+              scale: scale,
+              maxPixels: 1e12
+          });
+          
+      territotiesData = ee.List(territotiesData.get('groups'));
+      var areas = territotiesData.map(convert2table);
+      areas = ee.FeatureCollection(areas).flatten();
+      return areas;
+  };
+  
+  // perform per year 
+  var areas = years.map(
+      function (year) {
+          var image = asset_i.select('classification_' + year);
+          var areas = calculateArea(image, territory, geometry);
+          // set additional properties
+          areas = areas.map(
+              function (feature) {
+                  return feature.set('year', year);
+              }
+          );
+          return areas;
+      }
+  );
+  
+  areas = ee.FeatureCollection(areas).flatten();
+  
+  Export.table.toDrive({
+      collection: areas,
+      description: basename,
+      folder: driverFolder,
+      fileFormat: 'CSV'
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
