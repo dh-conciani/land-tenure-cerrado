6ca182a42fb1739f0a569a9b31fc58cfa7370a7d
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2022-09-23 11:33:26 -0300

    Update 02_parseData.R

--- 02_parseData.R
+++ 02_parseData.R
@@ -13,6 +13,8 @@ files <- list.files('./table/lcluc-tenure-state/raw', full.names= TRUE)
 tenure_dict <- read.csv('./dictionary/tenure-dict.csv', sep= ';')
 
 ## create recipe to receive data
+data <- as.data.frame(NULL)
+## for each fiel (state)
 for (i in 1:length(unique(files))) {
   ## read file i
   x <- read.csv(files[i])
@@ -22,16 +24,24 @@ for (i in 1:length(unique(files))) {
   x$state <- sapply(strsplit(basename(files[i]), split='_', fixed=TRUE),
                     function(x) (x[1]))
   
+  ## create recipe to translate each land tenure
+  recipe <- as.data.frame(NULL)
+  ## for each tenure id
+  for (j in 1:length(unique(x$tenure))) {
+    ## for each unique value, get mean in n levels
+    y <- subset(tenure_dict, Value == unique(x$tenure)[j])
+    ## select matched land tenure 
+    z <- subset(x, tenure == unique(x$tenure)[j])
+    ## apply tenure translation for each level
+    z$tenure_l1 <- gsub(paste0('^',y$Value,'$'), y$tenure.l1, z$tenure)
+    z$tenure_l2 <- gsub(paste0('^',y$Value,'$'), y$tenure.l2, z$tenure)
+    z$tenure_l3 <- gsub(paste0('^',y$Value,'$'), y$tenure.l3, z$tenure)
+    ## bind into recipe
+    recipe <- rbind(recipe, z)
+  }
+  
 }
 
 
-## for each 
-for (j in 1:length(unique(x$tenure))) {
-  ## for each unique value, get mean in n levels
-  y <- subset(tenure_dict, Value == unique(x$tenure)[j])
-  ## apply tenure translation 
-  
-  
-}
 
-  
+
