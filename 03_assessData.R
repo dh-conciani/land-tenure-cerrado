6d49ba93ab4ef57ea50056158e3028e7b8634ea4
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2022-09-26 13:37:03 -0300

    Update 03_assessData.R

--- 03_assessData.R
+++ 03_assessData.R
@@ -3,6 +3,8 @@
 
 ## get libraries
 library (ggplot2)
+library (treemapify)
+library(ggrepel)
 
 ## avoid scientific notation
 options(scipen=999)
@@ -48,4 +50,62 @@ ggplot(data= recipe,
   ylab('Porcentagem %') +
   coord_flip()
   
-  
+## get only native vegetation
+native <- subset(recipe, class == 'Vegetação Nativa')
+
+## compuite perc
+native$perc <- round(native$area/sum(native$area) * 100, digits= 2)
+
+## plot
+ggplot(data= native, mapping= aes(x= reorder(tenure, area), y= area/1e6)) +
+  geom_bar(stat='identity', fill= '#129912', alpha= 0.8) +
+  geom_text_repel(mapping=aes(label= paste0(round(area/1e6, digits=2), 'Mha - ', round(area/1e6, digits=1), '%')),
+            position = position_stack(vjust = 0.5), col= 'gray10') + 
+  theme_minimal() +
+  xlab(NULL) +
+  ylab('Área (Mha)') +
+  coord_flip()
+
+## compute loss of native vegetation
+## get 1985 and 2021 data
+x <- subset(data, year == 1985 & mapb_0 == 'Vegetação Nativa' |year == 2021 & mapb_0 == 'Vegetação Nativa')
+## aggregate per year
+y <- aggregate(x=list(area= x$area), by= list(class= x$mapb_0, year= x$year, tenure= x$tenure_l1), FUN='sum')
+## compute loss
+loss <- as.data.frame(NULL)
+for (i in 1:length(unique(y$tenure))) {
+  ## subset
+  z <- subset(y, tenure == unique(y$tenure)[i])
+  ## compute loss
+  z$loss <- z$area[1] - z$area[2]
+  ## get relative loss (cmparting total to 1985)
+  z$rel_loss <- round(z$loss/z$area * 100, digits=1)[1]
+  z <- z[2,]
+  ## bind
+  loss <- rbind(z, loss); rm(z)
+}
+
+## compuite perc in relation to the total
+loss$perc_tot <- round(loss$loss/sum(loss$loss) * 100, digits=2)
+
+## plot loss
+ggplot(data= loss, mapping= aes(x= reorder(tenure, area), y= loss/1e6)) +
+  geom_bar(stat='identity', fill= 'tomato1', alpha=0.8) +
+  geom_text_repel(mapping=aes(label= paste0(round(loss/1e6, digits=2), 'Mha - ', perc_tot, '%'))) +
+  coord_flip() +
+  theme_minimal() +
+  xlab(NULL) +
+  ylab('Área (Mha)')
+
+
+## plot relation among tenure and loss
+ggplot(data= loss, mapping= aes(x=area/1e6, y= loss/1e6)) +
+  geom_text_repel(mapping= aes(label=tenure), size=3) +
+  geom_point(mapping=aes(size= rel_loss, col= tenure),alpha=0.7) + 
+  theme_bw() +
+  scale_y_log10() +
+  #scale_x_log10() +
+  xlab('Vegetação nativa remanescente (Mha)') +
+  ylab('Perda liíquida de vegetação nativa (1985 - 2021, Mha)')
+
+
